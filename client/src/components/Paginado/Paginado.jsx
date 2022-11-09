e81beab55f4d1510ac7cf5e54ec4fbc95e94a92e
LourdesRosa1 <lourdesrosaa1@gmail.com>
2022-11-09 08:55:12 -0300

    Pokemon06

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from './Paginado.module.css'
 
-export default function Paginado({pokemonPerPage, allPokemons, currentPage, paginado }) {
+export default function Paginado({pokemonPerPage, allPokemons, paginado }) {
     const pageNumbers=[]
     for (let i=1; i<= Math.ceil(allPokemons/pokemonPerPage); i++) {
         pageNumbers.push(i)
